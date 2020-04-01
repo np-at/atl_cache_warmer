7a6519831792977f6c7a0e7fed3d96e36cc9acb4
np-at <noahpraskins@gmail.com>
2020-04-01 13:31:02 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='atl_cache_warmer',
-    version='0.13',
+    version='0.13.1',
     packages=['atl_cache_warmer'],
     entry_points={
         'console_scripts': [
